97b10dcd37e4d6ca64176569856be2800da68b87
Avadhi-Singhal <singhalavadhi2211@gmail.com>
2024-11-25 16:18:13 +0530

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,21 @@
+/**
+ * The Constant class contains static string constants used throughout the program. These constants help in 
+ * maintaining uniformity in messages displayed to the user and facilitate easy modifications without changing 
+ * the core logic of the program.
+ * 
+ * Owner : Avadhi-Singhal
+ * 
+ * Date of Creation : 25/11/2024
+ */
+
+public class Constant {
+	
+	public static String USER_PROMPT = "Enter a mathematical expression (e.g., a+b*(c-d)/e) or type 'exit' to quit: ";
+	
+	public static String EXIT_MSG = "\n Exiting...";
+	
+	public static String RESULT = "Result: ";
+	
+	public static String INVALID_EXPRESSION_MSG = "Invalid expression. Please try again.";
+
+}
